82060fb19c2fbb76a2cdddb3682416b3420fb65d
ShineShao <xiaoshaoqq@gmail.com>
2024-05-19 18:04:39 +0800

    fix: footer render

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -2,7 +2,7 @@ import cls from 'classnames';
 import style from './index.module.scss';
 
 const Footer = () => {
-  <div className={cls(style.footer, '__footer')}>footer</div>;
+  return <div className={cls(style.footer, '__footer')}>footer</div>;
 };
 
 export default Footer;
