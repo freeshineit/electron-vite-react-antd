ccac8845098d18e496e0aefe98e4f954e46c59eb
ShineShao <xiaoshaoqq@gmail.com>
2024-05-20 21:05:36 +0800

    chore: update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 /// <reference types="vite/client" />
+/// <reference types="./type/electron-updater.d.ts" />
 
 interface Window {
   // expose in the `electron/preload/index.ts`
