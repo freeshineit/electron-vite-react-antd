26d6448230715bf05f31c1ca80af47755d1ed21d
ShineShao <xiaoshaoqq@gmail.com>
2024-05-22 23:23:11 +0800

    feat: sqlite3

--- electron/sql/index.ts
+++ electron/sql/index.ts
@@ -1,17 +1,18 @@
 /* eslint-disable @typescript-eslint/promise-function-async */
 import { app } from 'electron';
-import sqlite3 from 'sqlite3';
+import Database from 'better-sqlite3';
 import path from 'path';
 import fse from 'fs-extra';
 import { ENV_CONFIG } from '../env_config';
 
 const TAG = '[sqlite3]';
+
 /**
  * @description 数据库操作 (VSCode 可以安装对应的插件访问数据库)
  */
 class Sql {
   private static _instance: Sql;
-  db: sqlite3.Database;
+  db;
 
   public static getInstance() {
     return (this._instance ??= new Sql());
@@ -23,23 +24,20 @@ class Sql {
     if (!fse.existsSync(DB_PATH)) {
       fse.ensureFileSync(DB_PATH);
     }
+
     console.log(TAG, 'DB_PATH', DB_PATH);
 
-    this.db = new sqlite3.Database(DB_PATH, (err) => {
-      if (err) {
-        console.error(TAG, err.message);
-      }
-      console.log(TAG, 'Connected to the database.');
-    });
+    this.db = new Database(DB_PATH, {});
   }
 
   open() {
     return new Promise<void>((resolve) => {
-      this.db.serialize(() => {
-        this.db.run('PRAGMA foreign_keys = ON');
-        console.log(TAG, 'Connected to the database.');
-        resolve();
-      });
+      // this.db.serialize(() => {
+      //   // this.db.run('PRAGMA foreign_keys = ON');
+      //   // console.log(TAG, 'Connected to the database.');
+      //   // resolve();
+      // });
+      resolve();
     });
   }
 
@@ -51,24 +49,25 @@ class Sql {
   // prettier-ignore
   createTable(sql: string) {
     return new Promise((resolve, reject) => {
-      this.db?.run(sql,
-        (err: Error | null) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(true);
-        },
-      );
+      // this.db?.run(sql,
+      //   (err: Error | null) => {
+      //     if (err) {
+      //       reject(err);
+      //     }
+      //     resolve(true);
+      //   },
+      // );
+      resolve(true);
     });
   }
 
   close() {
-    this.db?.close((err) => {
-      if (err) {
-        //
-        console.error(TAG, 'DB clone error: ', err.message);
-      }
-    });
+    // this.db?.close((err) => {
+    //   if (err) {
+    //     //
+    //     console.error(TAG, 'DB clone error: ', err.message);
+    //   }
+    // });
   }
 }
 
